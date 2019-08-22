9f67e7bcd4cbc08c2666abcf146da7387f05a973
Ariel Valle <avallehh@gmail.com>
2019-08-22 19:42:08 -0400

    Added logic to get song from Spotify and other operations

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 // console.log("Hello World");
 
@@ -6,19 +10,36 @@ var axios = require("axios");
 
 var operation = process.argv[2];
 
-var movieName = process.argv[3];
-// console.log(operation, movieName);
+
 
 function getMovieData() {
+    var movieName = process.argv[3];
     console.log(movieName)
     axios.get(`http://www.omdbapi.com/?t=${movieName}&apikey=7969af0b`).then(function(response){
     console.log(response)
     })
     // console.log(movieData)
 }
-
+function getSpotifySong() {
+    var songName = process.argv[3];
+    console.log(songName)
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data.tracks.items[0]); 
+      });
+}
 if (operation==='movie-this'){
     getMovieData()
 
+} else if(operation==='concert-this'){
+    console.log('concert-this requested')
+} else if (operation==='spotify-this-song'){
+    console.log('spotify requested')
+    getSpotifySong()
+} else if(operation==='do-what-it-says'){
+    console.log('do-what-it-says requested')
 }
-
+ 
